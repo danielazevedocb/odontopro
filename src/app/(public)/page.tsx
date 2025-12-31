c65028e1f22d510f1b72926261091a07b58fcfc4
danielazevedocb <daniel.santoscb@gmail.com>
2025-12-31 14:45:17 -0300

    feat: adiciona componente Hero na página inicial
    - Cria componente Hero com seção de apresentação
    - Adiciona título e descrição da plataforma
    - Implementa botão CTA 'Encontre uma Clínica'
    - Adiciona imagem ilustrativa do profissional de saúde (responsiva)
    - Integra Hero na página principal
    - Melhora layout da página com flex-col e min-h-screen

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from '@/app/(public)/_components/header';
+import { Hero } from '@/app/(public)/_components/hero';
 
 export default function Home() {
-  return(
-    <div>
+  return (
+    <div className=" flex flex-col min-h-screen">
       <Header />
+      <div>
+        <Hero />
+      </div>
     </div>
-  )
+  );
 }
