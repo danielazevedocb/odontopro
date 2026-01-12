19d79faca1049d2a1fda6a8a29c3efac228f5fbc
danielazevedocb <daniel.santoscb@gmail.com>
2026-01-12 15:57:07 -0300

    feat: adiciona configuração do Prisma e estrutura inicial

---
+++ src/lib/prisma.ts
@@ -0,0 +1,25 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+let prisma: PrismaClient;
+
+const adapter = new PrismaPg({ connectionString });
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    adapter,
+  });
+} else {
+  let globalWithPrisma = global as typeof globalThis & { prisma: PrismaClient };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient({
+      adapter,
+    });
+  }
+  prisma = globalWithPrisma.prisma;
+}
+
+export default prisma;
